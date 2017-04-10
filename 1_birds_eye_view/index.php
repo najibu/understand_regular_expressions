96aaf3c4c965e9b1c3270836822a0a0bef1aaa6e
najibu <ns.najibu@gmail.com>
2017-04-10 15:49:54 +0100

    Added birds eye view

---
+++ 1_birds_eye_view/index.php
@@ -0,0 +1,28 @@
+<?php 
+
+// Regex 
+
+// http://regexr.com/
+
+// Ex 1
+/\$NAME\$/g
+
+My name is $NAME$.
+
+# PHP
+$output = preg_replace('/\$NAME\$/', 'Najibu Nsubuga', $string);
+
+# Anything between the dollar sign
+/\$.+\$/g
+
+# Captial letters
+/\$[A-Z]+/$\g
+# end
+
+// Ex 2
+/((?:ht|f)tps?:\/\/([^\s]+))/g
+
+Visit https://najibunsubuga.me to view my work.
+
+# replace
+<a href="$1">$2</a>
