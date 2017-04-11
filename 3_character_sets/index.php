9c0340a3b23d798a5adc16abd024b1a3333f1bac
najibu <ns.najibu@gmail.com>
2017-04-11 12:41:01 +0100

    Added character sets

---
+++ 3_character_sets/index.php
@@ -0,0 +1,36 @@
+<?php 
+
+// Character sets 
+# Imagine that you need to match any of these words ['Born, Torn, Corn']. 
+# http://regexr.com/
+
+// Ex 1
+// Regex
+# /[BTC]orn/ig
+
+// Text 
+#Born
+#Torn
+#Corn
+#Worn -> not the above 3 characters /[^BTC]orn/ig
+
+
+// Ex 2
+// Matching 3 www or max 5
+#Regex
+# /[w]{3, 5}/ig
+# /[w]*/ig match 0 or more characters
+# /[w]+/ig match 1 or more characters
+# /[w]?/ig match 0 or 1 character
+
+// Text 
+# www
+
+
+// Ex 3 
+// Regex
+// . matches anything except line break
+# /www\.([^\.]+)/ig 
+
+// Text 
+# www.najibunsubuga.me
